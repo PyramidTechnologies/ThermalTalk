38af010dbc4194d49c02a7ba410c81481f5269fd
Cory Todd <cory@pyramidacceptors.com>
2017-04-26 15:28:49 -0700

    Create base serial port
    Use as abstract base between Phoenix and Reliance

---
+++ ThermalTalk/Common/BaseSerialPort.cs
@@ -0,0 +1,202 @@
+﻿/*
+MIT License
+
+Copyright (c) 2017 Pyramid Technologies
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+ */
+namespace ThermalTalk
+{
+    using System.IO.Ports;
+    using System.Threading;
+
+    abstract class BaseSerialPort : ISerialConnection
+    {
+        #region Fields
+        protected readonly SerialPort _mPort;
+        protected int _mReadTimeout;
+        protected int _mWriteTimeout;
+        #endregion
+
+        #region Constructor
+        protected BaseSerialPort(string portName, 
+            int baud, 
+            int databits, 
+            Parity parity, 
+            StopBits stopbits, 
+            Handshake handshake)
+        {
+            Name = portName;
+
+            _mPort = new SerialPort(portName, baud, parity, databits, stopbits);
+            _mPort.Encoding = System.Text.Encoding.GetEncoding("Windows-1252");
+            _mPort.Handshake = handshake;
+            _mPort.WriteTimeout = _mWriteTimeout = 500;
+            _mPort.ReadTimeout = _mReadTimeout = 500;
+        }
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Gets the serial port name for this connection
+        /// </summary>
+        public string Name { get; private set; }
+
+        /// <summary>
+        /// Gets or Sets the read timeout in milliseconds
+        /// </summary>
+        public int ReadTimeoutMS
+        {
+            get { return _mReadTimeout; }
+            set
+            {
+                _mReadTimeout = value;
+                _mPort.ReadTimeout = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or Sets the write timeout in milliseconds
+        /// </summary>
+        public int WriteTimeoutMS
+        {
+            get { return _mWriteTimeout; }
+            set
+            {
+                _mWriteTimeout = value;
+                _mPort.WriteTimeout = value;
+            }
+        }
+
+        #endregion
+
+        public ReturnCode Open()
+        {
+            if (_mPort.IsOpen) return ReturnCode.Success;
+
+            try
+            {
+                _mPort.Open();
+                return ReturnCode.Success;
+            } 
+            catch(System.IO.IOException)
+            {
+                return ReturnCode.ConnectionNotFound;
+            }
+            catch(System.AccessViolationException)
+            {
+                return ReturnCode.ConnectionAlreadyOpen;
+            }
+        }
+
+        public ReturnCode Close()
+        {
+            if (!_mPort.IsOpen) return ReturnCode.Success;
+
+            try
+            {
+                _mPort.Close();
+                return ReturnCode.Success;
+            }
+            catch
+            {
+                return ReturnCode.ExecutionFailure;
+            }
+        }
+
+
+        public int Write(byte[] payload)
+        {
+            return WritePort(payload);
+        }
+
+        public byte[] Read(int n)
+        {
+            return ReadPort(n);
+        }
+
+        public void Dispose()
+        {
+            if(_mPort != null)
+            {
+                Close();
+                _mPort.Dispose();
+            }
+        }    
+
+        #region Protected
+        /// <summary>
+        /// Writes data to the printer serial port
+        /// </summary>
+        /// <param name="data">Data to send</param>
+        /// <returns>Number of bytes written</returns>
+        protected int WritePort(byte[] data)
+        {
+            try
+            {
+                // Split into chunks to avoid overrunning target buffer
+                foreach (var s in data.Split(256))
+                {
+                    _mPort.Write(s, 0, s.Length);
+                    Thread.Sleep(10);
+                }
+
+                return data.Length;
+            }
+            catch
+            { 
+                return 0; 
+            }
+        }
+
+        /// <summary>
+        /// Reads count bytes from serial port. If count bytes are unavailable, this
+        /// function will block until the read times out. If there is an exception or
+        /// not all expected data is received, an empty buffer will be returned.
+        /// </summary>
+        /// <param name="count">Number of bytes to read from port.</param>
+        /// <returns>Bytes read from port.</returns>
+        protected byte[] ReadPort(int count)
+        {
+
+            byte[] buff = new byte[count];
+
+            try
+            {
+
+                // Attempt to read count bytes
+                var read = _mPort.Read(buff, 0, count);
+
+                // If we don't get enough, reset buffer to 0 length
+                if (read != count)
+                {
+                    buff = new byte[0];
+                }
+
+            }
+            catch
+            {
+                buff = new byte[0];
+            }
+
+            return buff;
+        }
+        #endregion
+    }
+}
--- ThermalTalk/Reliance/RelianceSerialPort.cs
+++ ThermalTalk/Reliance/RelianceSerialPort.cs
@@ -26,25 +26,18 @@ namespace ThermalTalk
     using System;
     using System.IO.Ports;
     using System.Runtime.InteropServices;
-    using System.Threading;
 
     /// <summary>
     /// Reliance serial port implementation 
     /// </summary>
-    class RelianceSerialPort : ISerialConnection
+    class RelianceSerialPort : BaseSerialPort
     {
         #region Default SerialPort Params
-        const int DefaultBaudRate = 19200;
-        const int DefaultDatabits = 8;
-        const Parity DefaultParity = Parity.None;
-        const StopBits DefaultStopbits = StopBits.One;
-        const Handshake DefaultHandshake = Handshake.None;
-        #endregion
-
-        #region Fields
-        private readonly SerialPort _mPort;
-        private int _mReadTimeout;
-        private int _mWriteTimeout;
+        private const int DefaultBaudRate = 19200;
+        private const int DefaultDatabits = 8;
+        private const Parity DefaultParity = Parity.None;
+        private const StopBits DefaultStopbits = StopBits.One;
+        private const Handshake DefaultHandshake = Handshake.None;
         #endregion
 
         #region Constructor
@@ -53,114 +46,10 @@ namespace ThermalTalk
         { }
 
         public RelianceSerialPort(string portName, int baud)
-            : this(portName, baud, DefaultDatabits, DefaultParity, DefaultStopbits, DefaultHandshake)
+            : base(portName, baud, DefaultDatabits, DefaultParity, DefaultStopbits, DefaultHandshake)
         { }
 
-        public RelianceSerialPort(string portName, 
-            int baud, 
-            int databits, 
-            Parity parity, 
-            StopBits stopbits, 
-            Handshake handshake)
-        {
-            Name = portName;
-
-            _mPort = new SerialPort(portName, baud, parity, databits, stopbits);
-            _mPort.Encoding = System.Text.Encoding.GetEncoding("Windows-1252");
-            _mPort.Handshake = handshake;
-            _mPort.WriteTimeout = _mWriteTimeout = 500;
-            _mPort.ReadTimeout = _mReadTimeout = 500;
-        }
         #endregion
-
-        #region Properties
-        /// <summary>
-        /// Gets the serial port name for this connection
-        /// </summary>
-        public string Name { get; private set; }
-
-        /// <summary>
-        /// Gets or Sets the read timeout in milliseconds
-        /// </summary>
-        public int ReadTimeoutMS
-        {
-            get { return _mReadTimeout; }
-            set
-            {
-                _mReadTimeout = value;
-                _mPort.ReadTimeout = value;
-            }
-        }
-
-        /// <summary>
-        /// Gets or Sets the write timeout in milliseconds
-        /// </summary>
-        public int WriteTimeoutMS
-        {
-            get { return _mWriteTimeout; }
-            set
-            {
-                _mWriteTimeout = value;
-                _mPort.WriteTimeout = value;
-            }
-        }
-
-        #endregion
-
-        public ReturnCode Open()
-        {
-            if (_mPort.IsOpen) return ReturnCode.Success;
-
-            try
-            {
-                _mPort.Open();
-                return ReturnCode.Success;
-            } 
-            catch(System.IO.IOException)
-            {
-                return ReturnCode.ConnectionNotFound;
-            }
-            catch(System.AccessViolationException)
-            {
-                return ReturnCode.ConnectionAlreadyOpen;
-            }
-        }
-
-        public ReturnCode Close()
-        {
-            if (!_mPort.IsOpen) return ReturnCode.Success;
-
-            try
-            {
-                _mPort.Close();
-                return ReturnCode.Success;
-            }
-            catch
-            {
-                return ReturnCode.ExecutionFailure;
-            }
-        }
-
-
-        public int Write(byte[] payload)
-        {
-            return WritePort(payload);
-        }
-
-        public byte[] Read(int n)
-        {
-            return ReadPort(n);
-        }
-
-        public void Dispose()
-        {
-            if(_mPort != null)
-            {
-                Close();
-                _mPort.Dispose();
-            }
-        }
-
 
         /// <summary>
         /// Write raw data to printer through its Windows print handle
@@ -182,6 +71,7 @@ namespace ThermalTalk
 
             return result ? ReturnCode.Success : ReturnCode.ExecutionFailure;
         }
+     
         /// <summary>
         /// Reads count bytes from printer
         /// </summary>
@@ -206,65 +96,6 @@ namespace ThermalTalk
             }
 
             return returnbytes;
-        }
-
-        #region Private
-        /// <summary>
-        /// Writes data to the printer serial port
-        /// </summary>
-        /// <param name="data">Data to send</param>
-        /// <returns>Number of bytes written</returns>
-        private int WritePort(byte[] data)
-        {
-            try
-            {
-                // Split into chunks to avoid overrunning target buffer
-                foreach (var s in data.Split(256))
-                {
-                    _mPort.Write(s, 0, s.Length);
-                    Thread.Sleep(10);
-                }
-
-                return data.Length;
-            }
-            catch
-            { 
-                return 0; 
-            }
-        }
-
-        /// <summary>
-        /// Reads count bytes from serial port. If count bytes are unavailable, this
-        /// function will block until the read times out. If there is an exception or
-        /// not all expected data is received, an empty buffer will be returned.
-        /// </summary>
-        /// <param name="count">Number of bytes to read from port.</param>
-        /// <returns>Bytes read from port.</returns>
-        private byte[] ReadPort(int count)
-        {
-
-            byte[] buff = new byte[count];
-
-            try
-            {
-
-                // Attempt to read count bytes
-                var read = _mPort.Read(buff, 0, count);
-
-                // If we don't get enough, reset buffer to 0 length
-                if (read != count)
-                {
-                    buff = new byte[0];
-                }
-
-            }
-            catch
-            {
-                buff = new byte[0];
-            }
-
-            return buff;
-        }
-        #endregion
+        }   
     }
 }
