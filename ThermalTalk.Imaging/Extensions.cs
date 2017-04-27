5419c9a99170568963b079800f1ea60c5e4f0ec1
Cory Todd <cory@pyramidacceptors.com>
2017-04-27 09:01:58 -0700

    Add license region wrapper to all headers

--- ThermalTalk.Imaging/Extensions.cs
+++ ThermalTalk.Imaging/Extensions.cs
@@ -1,4 +1,5 @@
-﻿/*
+﻿#region Copyright & License
+/*
 MIT License
 
 Copyright (c) 2017 Pyramid Technologies
@@ -21,6 +22,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
  */
+#endregion
 namespace ThermalTalk.Imaging
 {
     using System;
