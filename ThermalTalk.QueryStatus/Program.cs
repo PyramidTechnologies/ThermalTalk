8315bbda95c35c619f01d497e34612266f2b0b19
Cody Gillespie <cgillespie@pyramidacceptors.com>
2020-07-06 08:24:47 -0700

    Added quick console app
    This allows us to send a get status command and print to console each of the responses

---
+++ ThermalTalk.QueryStatus/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ThermalTalk.QueryStatus
+{
+    using System;
+
+    /// <summary>
+    /// Simple console app to query 
+    /// </summary>
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            const string reliancePort = "COM12";
+
+            using (var printer = new ReliancePrinter(reliancePort))
+            {
+                foreach (StatusTypes type in Enum.GetValues(typeof(StatusTypes)))
+                {
+                    var status = printer.GetStatus(type);
+
+                    // name of status to query
+                    Console.WriteLine(Enum.GetName(typeof(StatusTypes), type));
+                    
+                    // status
+                    Console.WriteLine(status.ToJSON());
+                }
+            }
+        }
+    }
+}
