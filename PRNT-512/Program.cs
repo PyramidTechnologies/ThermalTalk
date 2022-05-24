e6f604462278bc08273e2d6a00e9d91f50812746
codygillespie <cgillespie@pyramidacceptors.com>
2022-05-24 07:30:38 -0700

    Sample barcode failing project
    Add a project to show the text after barcode print failing

---
+++ PRNT-512/Program.cs
@@ -0,0 +1,27 @@
+﻿// Create document
+
+using ThermalTalk;
+
+using var printer = new ReliancePrinter("COM3");
+
+var document = new StandardDocument
+{
+    CodePage = CodePages.CP1252,
+};
+
+// create and add barcode
+var barcode = new TwoDBarcode(TwoDBarcode.Flavor.Reliance)
+{
+    EncodeThis = "1FBxB5atGb4hLpfRDD7h8cnAH1unU9HT7i"
+};
+
+document.Sections.Add(new BarcodeSection(barcode));
+
+// add in text subsequent to barcode
+document.Sections.Add(new StandardSection
+{
+    Content = "1FBxB5atGb4hLpfRDD7h8cnAH1unU9HT7i",
+});
+
+printer.PrintDocument(document);
+printer.FormFeed();
