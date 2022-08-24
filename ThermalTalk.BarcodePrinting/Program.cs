591a509a31ed7cd5bfc128c1b0e62e9720baa32d
codygillespie <cgillespie@pyramidacceptors.com>
2022-08-24 15:06:07 -0700

    Modify script for debugging raster image

--- ThermalTalk.BarcodePrinting/Program.cs
+++ ThermalTalk.BarcodePrinting/Program.cs
@@ -7,18 +7,18 @@ namespace ThermalTalk.BarcodePrinting
     {
         public static void Main(string[] args)
         {
+            
+            // Use index 1...5
+            const int indexForQrCode = 1;            
+            // Change for your printer:
+            const string portName = "COM26";
+
             // Sample 500 character string to encode
-            const string stringToEncode = "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo " +
-                                          "ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis " +
-                                          "dis parturient montes, nascetur ridiculus mus. Donec quam felis, " +
-                                          "ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa " +
-                                          "quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, " +
-                                          "arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. " +
-                                          "Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibu";
-            
+            const string stringToEncode1 = "HUv0hBlbCPPFXJy3U5b5rI+RfvpiKH1IJ6vEsrGb4AvaAltdl5k3W9SlHYOVBmrQ;BwIAAACkAABSU0EygAEAAAEAAQD7SQxChCQ2J/y5HYjzeGtNjz00NmLC45ceEnhTf/EXbdvHWgNlB2X7skdDq9xnS7btNIqX8B1k2x2JwSUw/QrIOKVka51xVuWHxTA1yG5BA2NbdfJMaRZaKclxpefcfMs1KxHtldR76hfnyQCuyEMgvJC3mAp3d8/JiyFBqrbPqcnuApKAb85eJg6mEfuJishdJAiVQ3FjAiqbWpdhBFNz37+eeouNSwbJFgVeodP6qpUNAFwX+HKIjdmzA4o3sLfLDHDAMXduKLCrsb1mZ7TTa1++P7rSuVo=";
+
             // Connect to printer - CHANGE THIS TO YOUR PRINTER'S COM PORT
-            var printer = new PhoenixPrinter("COM14");
-            
+            var printer = new PhoenixPrinter(portName);
+
             var document = new StandardDocument
             {
                 CodePage = CodePages.CPSPACE,
@@ -26,15 +26,15 @@ namespace ThermalTalk.BarcodePrinting
 
             var headerSection = new StandardSection
             {
-                Content = "Header",
+                Content = "REGISTER MACHINE",
                 Justification = FontJustification.JustifyCenter,
-                HeightScalar = FontHeighScalar.h2,
-                WidthScalar = FontWidthScalar.w2,
-                Effects = FontEffects.Bold,
-                Font = ThermalFonts.A,
+                HeightScalar = FontHeighScalar.h1,
+                WidthScalar = FontWidthScalar.w1,
+                Effects = FontEffects.Bold, 
+                Font = ThermalFonts.C,
                 AutoNewline = true,
             };
-            
+
             var storeIdSection = new StandardSection
             {
                 Content = "# STORE: 1234",
@@ -45,20 +45,56 @@ namespace ThermalTalk.BarcodePrinting
                 Font = ThermalFonts.A,
                 AutoNewline = true,
             };
+            
+            var terminalIdSection = new StandardSection
+            {
+                Content = "# Terminal: 1234",
+                Justification = FontJustification.JustifyCenter,
+                HeightScalar = FontHeighScalar.h2,
+                WidthScalar = FontWidthScalar.w2,
+                Effects = FontEffects.Bold,
+                Font = ThermalFonts.A,
+                AutoNewline = true,
+            };
+            
+            var dateSection = new StandardSection
+            {
+                Content = "Date: 1234",
+                Justification = FontJustification.JustifyCenter,
+                HeightScalar = FontHeighScalar.h2,
+                WidthScalar = FontWidthScalar.w2,
+                Effects = FontEffects.Bold,
+                Font = ThermalFonts.A,
+                AutoNewline = true,
+            };
+            
+            var timeSection = new StandardSection
+            {
+                Content = "Time: 1234",
+                Justification = FontJustification.JustifyCenter,
+                HeightScalar = FontHeighScalar.h2,
+                WidthScalar = FontWidthScalar.w2,
+                Effects = FontEffects.Bold,
+                Font = ThermalFonts.A,
+                AutoNewline = true,
+            };
 
             // add text
             document.Sections.Add(headerSection);
+            document.Sections.Add(storeIdSection);
+            document.Sections.Add(terminalIdSection);
+            document.Sections.Add(dateSection);
+            document.Sections.Add(timeSection);
 
             // Use QRCoder library to generate QR code image
             var qrGenerator = new QRCodeGenerator();
-            var qrData = qrGenerator.CreateQrCode(stringToEncode, QRCodeGenerator.ECCLevel.Q);
+            var qrData = qrGenerator.CreateQrCode(stringToEncode1, QRCodeGenerator.ECCLevel.Q);
             var qrCode = new QRCode(qrData);
             var qrCodeImage = qrCode.GetGraphic(3);
 
             // set printer image
             var printerImage = new PrinterImage(qrCodeImage);
-            printer.SetImage(printerImage, document, 1);
-            document.Sections.Add(storeIdSection);
+            printer.SetImage(printerImage, document, indexForQrCode);
 
             // Print sample image
             printer.PrintDocument(document);
