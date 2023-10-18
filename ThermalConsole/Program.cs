919476d0fc23af22aef9ea6c735b86ee7f940640
Cristion Dominguez <crdominguez@pyramidacceptors.com>
2023-10-18 14:10:31 -0700

    Increase barcode size in sample

--- ThermalConsole/Program.cs
+++ ThermalConsole/Program.cs
@@ -32,7 +32,7 @@ app.Configure(config =>
 // args = new[]
 // {
 //     "printcode", "reliance", "COM11",
-//     "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa \" +\n                              \"quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibu"
+//     "Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibu"
 // };
 // args = new[] { "capture", "reliance", "COM11" };
 
