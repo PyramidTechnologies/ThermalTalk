6b6a873c9d219cd0453047634349d9f89a858d78
Cristion Dominguez <crdominguez@pyramidacceptors.com>
2023-10-23 14:13:26 -0700

    Check if NuGet files exist in deploy.ps1

--- nuget/deploy.ps1
+++ nuget/deploy.ps1
@@ -4,14 +4,18 @@
 #
 
 $PackageFiles = Get-ChildItem -Filter *.nupkg -Recurse
-foreach ($File in $PackageFiles) {
-    Remove-Item $PackageFile.FullName
+if ($null -ne $PackageFiles) {
+    foreach ($File in $PackageFiles) {
+        Remove-Item $File.FullName
+    }
 }
 
 dotnet pack ThermalTalk/ThermalTalk.csproj --configuration Release
 dotnet pack ThermalTalk.Imaging/ThermalTalk.Imaging.csproj --configuration Release
 
 $PackageFiles = Get-ChildItem -Filter *.nupkg -Recurse
-foreach ($File in $PackageFiles) {
-    dotnet nuget push $File.FullName -source https://www.nuget.org/api/v2/package
+if ($null -ne $PackageFiles) {
+    foreach ($File in $PackageFiles) {
+        dotnet nuget push $File.FullName --source https://www.nuget.org/api/v2/package
+    }
 }
